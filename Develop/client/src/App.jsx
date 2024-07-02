384f7563cbdce13eef59b5446206bcd5fbf774e7
Britney Padron <britneypadr@gmail.com>
2024-07-01 21:08:40 -0500

    Set up Apollo Client

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
+import App from './App';
 
-import Navbar from './components/Navbar';
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-}
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
 
-export default App;
+export default ApolloApp;
